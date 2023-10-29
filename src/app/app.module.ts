ee81d720b540d908e65e58bfe0827f38288a01cf
Gregsai <gregory.sailly26@gmail.com>
2023-10-29 19:33:17 +0100

    Main Page & route file initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import routeConfig from './routes';
+
 import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { CoursesComponent } from './components/courses/courses.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    RouterModule.forRoot(routeConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
