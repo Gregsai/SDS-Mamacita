2df14b451dff9d10b37c2db12dd0392cf724703a
Gregsai <gregory.sailly26@gmail.com>
2024-01-13 01:01:08 +0100

    implementation of comment section both add an view

--- src/app/components/course-details-components/course-details-comments/course-details-comments.component.ts
+++ src/app/components/course-details-components/course-details-comments/course-details-comments.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommentsService } from 'src/app/services/comments.service';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-details-comments',
   templateUrl: './course-details-comments.component.html',
   styleUrls: ['./course-details-comments.component.css']
 })
-export class CourseDetailsCommentsComponent {
+export class CourseDetailsCommentsComponent implements OnInit {
+  errorMessage: string = '';
+  displayErrorMessage: boolean = false;
+  userId: string | null = null;
+  userEmail: string | null = null;
+  courseId: string | null = null;
+  displayMessage: boolean = false;
+  message: string = '';
+  comments: any[] = [];
 
+  @ViewChild('commentContent') commentContent!: ElementRef;
+
+  constructor(
+    private commentsService: CommentsService,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.courseId = this.route.snapshot.paramMap.get('id');
+    this.getComments();
+  }
+
+  addComment(): void {
+    if (!this.userService.isLoggedIn()) {
+      this.errorMessage = 'You must be logged in to add a comment';
+      this.displayErrorMessage = true;
+      setTimeout(() => {
+        this.displayErrorMessage = false;
+        this.errorMessage = '';
+      }, 5000);
+      return;
+    } else {
+      this.userId = this.userService.getUserId();
+      this.userEmail = this.userService.getEmail();
+      const message = this.commentContent.nativeElement.innerText;
+
+      if (!message.trim()) {
+        this.errorMessage = 'Please enter a valid comment';
+        this.displayErrorMessage = true;
+        setTimeout(() => {
+          this.displayErrorMessage = false;
+          this.errorMessage = '';
+        }, 5000);
+        return;
+      }
+
+      if (this.courseId && this.userId) {
+        this.commentsService.addComment(this.userId,this.userEmail, this.courseId, message).subscribe(
+          () => {
+            this.message = 'Comment successfully added';
+            this.displayMessage = true;
+            this.commentContent.nativeElement.innerText = '';
+            setTimeout(() => {
+              this.displayMessage = false;
+              this.message = '';
+            }, 5000);
+            return;
+          },
+          error => {
+            console.error('Error adding comment:', error);
+            this.errorMessage = 'An error occurred while adding the comment';
+            this.displayErrorMessage = true;
+            setTimeout(() => {
+              this.displayErrorMessage = false;
+              this.errorMessage = '';
+            }, 5000);
+            return;
+          }
+        );
+      }
+    }
+  }
+
+  getComments(): void {
+    if (this.courseId) {
+      this.commentsService.getCommentsForCourse(this.courseId).subscribe(
+        (comments: any[]) => {
+          this.comments = comments;
+        },
+        error => {
+          console.error('Error retrieving comments:', error);
+        }
+      );
+    }
+  }
 }
--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, doc, query, setDoc, getDoc, deleteDoc, getDocs, DocumentData, where, addDoc, updateDoc } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable, of, from } from 'rxjs';
+import { Firestore, collection, doc, setDoc, Timestamp, getDocs, query, onSnapshot } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable, from, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { UserService } from './user.service';
 import { CoursesService } from './courses.service';
@@ -13,14 +13,48 @@ export class CommentsService {
   constructor(
     private firestore: Firestore,
     private coursesService: CoursesService,
-  ) {
+  ) { }
 
+  addComment(userId: string, userEmail: string, courseId: string, message: string): Observable<void> {
+    const commentData = {
+      authorId: userId,
+      authorEmail: userEmail,
+      courseId: courseId,
+      date: Timestamp.now(),
+      message: message
+    };
+
+    const commentCollection = collection(this.firestore, `comments/${courseId}/course_comments`);
+
+    return from(setDoc(doc(commentCollection), commentData));
   }
 
-  getCollectionData(collectionName: string): Observable<any[]> {
-    return collectionData(collection(this.firestore, collectionName), { idField: 'id' });
+  getCommentsForCourse(courseId: string): Observable<any[]> {
+    const commentCollection = collection(this.firestore, `comments/${courseId}/course_comments`);
+    const commentsQuery = query(commentCollection);
+
+    return new Observable((observer) => {
+      const unsubscribe = onSnapshot(commentsQuery, (snapshot) => {
+        const comments: any[] = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          const comment = {
+            authorId: data['authorId'],
+            authorEmail: data['authorEmail'],
+            courseId: data['courseId'],
+            timestamp: data['date'].toDate().getTime(),
+            date: data['date'].toDate().toLocaleDateString(),
+            time: data['date'].toDate().toLocaleTimeString(),
+            message: data['message']
+          };
+          comments.push(comment);
+        });
+
+        comments.sort((a, b) => a.timestamp - b.timestamp);
+        observer.next(comments);
+      });
+
+      return () => unsubscribe();
+    });
   }
-
-
-
 }
