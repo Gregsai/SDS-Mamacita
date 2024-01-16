a37ddfdd5426b230859d5e457908e5555c9d4531
DreynN <nicolas.vongsanti@sfr.fr>
2024-01-16 12:44:49 +0100

    Create activity Feature

--- src/app/components/cultural-ressources/cultural-ressources-main-content/cultural-ressources-main-content.component.ts
+++ src/app/components/cultural-ressources/cultural-ressources-main-content/cultural-ressources-main-content.component.ts
@@ -1,24 +1,20 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CulturalressourcesService } from 'src/app/services/culturalressources.service';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-cultural-ressources',
-  templateUrl: './cultural-ressources.component.html',
-  styleUrls: ['./cultural-ressources.component.css']
+  selector: 'app-cultural-ressources-main-content',
+  templateUrl: './cultural-ressources-main-content.component.html',
+  styleUrls: ['./cultural-ressources-main-content.component.css']
 })
-export class CulturalRessourcesComponent {
+export class CulturalRessourcesMainContentComponent {
   activities$: Observable<any[]> | undefined;
-  isLoggedIn: boolean = false;
 
   constructor(
     private culturalressourcesService: CulturalressourcesService,
-    private userService: UserService,
   ) { }
 
   ngOnInit(): void {
     this.activities$ = this.culturalressourcesService.activities$;
-    this.isLoggedIn = this.userService.isLoggedIn();
   }
 }
